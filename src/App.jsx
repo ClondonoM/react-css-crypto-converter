ec2d17ca809e2afd86db76212a449f191063217d
Carlos Londono <c.londono.m@gmail.com>
2022-06-29 19:26:36 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import styled from '@emotion/styled';
+
+const Heading = styled.h1`
+  font-family: 'Montserrat', sans-serif;
+  color: #ffffff;
+`;
+
+function App() {
+  return <Heading>Crypto Converter</Heading>;
+}
+
+export default App;
